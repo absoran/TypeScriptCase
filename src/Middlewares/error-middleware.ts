a70c8780fbff4902713639eee72234a942169129
absoran <absoran97@gmail.com>
2024-01-27 01:26:54 +0300

    adds error handle middleware

---
+++ src/Middlewares/error-middleware.ts
@@ -0,0 +1,18 @@
+import { BaseHttpException } from "../Utils/http-exceptions";
+import { NextFunction, Request, Response } from "express";
+
+function errorMiddleware(
+    error: BaseHttpException,
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const status = error.status || 500;
+    const message = error.message || "Something went wrong";
+    response.status(status).json({
+        status,
+        message,
+    });
+}
+
+export default errorMiddleware;
