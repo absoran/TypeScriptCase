175d6f5fd4e5381af47a2823c7c7a2ef6f15a5e8
absoran <absoran97@gmail.com>
2024-01-28 19:43:21 +0300

    adds variant repo to app constructor

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import {ProductService} from "./Service/product-service";
 import {ProductRepository} from "./Repository/product-repository";
-import App from "./server";
-
+import {ProductVariantRepository} from "./Repository/product-variant-repository.ts";
 import {ProductControllers} from "./Controller/product-controllers";
 
+import App from "./server";
 
+
+// build app
 const app = new App([
     new ProductControllers(
         new ProductService(
             new ProductRepository(),
+            new ProductVariantRepository()
         )
     ),
 ]);
